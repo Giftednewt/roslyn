05ab9e0a7bad25544c5dd0c61fb1c5ebe260128c
Darren Blaby <giftednewt@hotmail.com>
2016-02-07 21:51:22 +0000

    implement code fix provider and action

--- src/Features/CSharp/Portable/CodeFixes/AddMissingSwitchLabels/AddMissingSwitchLabelsCodeFixProvider.AddMissingLabelsAction.cs
+++ src/Features/CSharp/Portable/CodeFixes/AddMissingSwitchLabels/AddMissingSwitchLabelsCodeFixProvider.AddMissingLabelsAction.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis.Formatting;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,7 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.AddMissingSwitchLabels
         private class AddMissingLabelsAction : CodeActions.CodeAction
         {
             private Document _document;
-            private SyntaxNode _node;
+            private SwitchStatementSyntax _node;
 
             public override string Title
             {
@@ -23,7 +25,7 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.AddMissingSwitchLabels
                 }
             }
 
-            public AddMissingLabelsAction(Document document, SyntaxNode node)
+            public AddMissingLabelsAction(Document document, SwitchStatementSyntax node)
             {
                 _document = document;
                 _node = node;
@@ -33,17 +35,42 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.AddMissingSwitchLabels
             {
                 var root = await _document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
 
-                var newNode = GetNewNode(_document, _node, cancellationToken);
+                var newNode = await GetNewNode(_document, _node, cancellationToken).ConfigureAwait(false);
                 var newRoot = root.ReplaceNode(_node, newNode);
 
                 return _document.WithSyntaxRoot(newRoot);
             }
 
-            private SyntaxNode GetNewNode(Document document, SyntaxNode node, CancellationToken cancellationToken)
+            private async Task<SyntaxNode> GetNewNode(Document document, SwitchStatementSyntax node, CancellationToken cancellationToken)
             {
-                SyntaxNode newNode = node;
+                var missingLabels = await GetMissingLabels(node, cancellationToken).ConfigureAwait(false);
+                SyntaxNode newNode = node.AddSections(missingLabels);
 
                 return newNode.WithAdditionalAnnotations(Formatter.Annotation);
+            }
+
+            private async Task<SwitchSectionSyntax> GetMissingLabels(SwitchStatementSyntax node, CancellationToken cancellationToken)
+            {
+                var semanticModel = await _document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+
+                var symbol = semanticModel.GetSymbolInfo(node.Expression);
+
+                var type = symbol.Symbol.GetSymbolType();
+
+                var members = type.GetMembers().OfType<IFieldSymbol>().ToDictionary(k => k.ConstantValue, f => SyntaxFactory.CaseSwitchLabel(SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, SyntaxFactory.IdentifierName(type.Name), SyntaxFactory.IdentifierName(f.Name))));
+
+                foreach (var label in node.Sections.SelectMany(x=> x.Labels.OfType<CaseSwitchLabelSyntax>()))
+                {
+                    var labelValueSymbol = semanticModel.GetSymbolInfo(label.Value,cancellationToken);
+                    var fieldSymbol = labelValueSymbol.Symbol as IFieldSymbol;
+                    if(members.ContainsKey(fieldSymbol.ConstantValue))
+                    {
+                        members.Remove(fieldSymbol.ConstantValue);
+                    }
+                }
+
+                //members.Except(switchStatement.Sections.SelectMany(s => s.Labels.OfType<CaseSwitchLabelSyntax>().Select(l => semanticModel.GetConstantValue(l.Value))));
+                return SyntaxFactory.SwitchSection(SyntaxFactory.List<SwitchLabelSyntax>(members.Values), SyntaxFactory.List<StatementSyntax>(new[] { SyntaxFactory.BreakStatement() }));
             }
         }
 
--- src/Features/CSharp/Portable/CodeFixes/AddMissingSwitchLabels/AddMissingSwitchLabelsCodeFixProvider.cs
+++ src/Features/CSharp/Portable/CodeFixes/AddMissingSwitchLabels/AddMissingSwitchLabelsCodeFixProvider.cs
@@ -6,13 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Shared.Extensions;
 
 namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.AddMissingSwitchLabels
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = PredefinedCodeFixProviderNames.AddMissingCaseLabels), Shared]
     internal partial class AddMissingSwitchLabelsCodeFixProvider : CodeFixProvider
     {
-        internal const string IDE0006 = "IDE0006"; // 'SomeClass.SomeMember' hides inherited member 'SomeClass.SomeMember'. Use the new keyword if hiding was intended.
+        internal const string IDE0006 = "IDE0006";
 
         public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(IDE0006);
 
@@ -21,10 +23,14 @@ namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.AddMissingSwitchLabels
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            context.RegisterCodeFix(new AddMissingLabelsAction(context.Document, root), context.Diagnostics);
-
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var token = root.FindToken(diagnosticSpan.Start);
+            var originalNode = token.GetAncestor<SwitchStatementSyntax>();
+
+            context.RegisterCodeFix(new AddMissingLabelsAction(context.Document, originalNode), context.Diagnostics);
+
         }
     }
 }
